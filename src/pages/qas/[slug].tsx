d4ce4e520d819d00e2559c87a8e947f63decf8d5
jlopinto <julien.lopinto@gmail.com>
2020-05-23 00:41:53 +0200

    Feat: QA

---
+++ src/pages/qas/[slug].tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { useQuery, gql } from "@apollo/client";
+import { Heading, Stack, Text } from "@chakra-ui/core";
+
+const QA_QUERY = gql`
+  query QAQuery($id: ID!) {
+    qA(where: { id: $id }) {
+        question
+        answershort
+        answermedium
+        id
+    }
+  }
+`;
+
+const Qa = function Qa(props) {
+  const router = useRouter();
+
+  const { data, loading, error } = useQuery<any, any>(QA_QUERY, {
+    variables: {
+      // @ts-ignore
+      id: router.query.slug,
+    },
+  });
+
+  console.log({ data, loading });
+  if (loading) return <p>loading</p>;
+  if (error) return <p> error</p>;
+  const {
+    qA: { question, answershort, answermedium },
+  } = data;
+  return (
+    <div>
+      <Stack spacing={8}>
+        <Heading>{question}</Heading>
+        <Text>{answershort}</Text>
+        <Text>{answermedium}</Text>
+      </Stack>
+    </div>
+  );
+};
+
+export default Qa;
