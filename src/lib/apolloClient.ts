3bc921c8f6b7050fab200f2c2c6a2283ed81f1a1
Frederik Eychenie <frederik@graphcms.com>
2020-06-09 19:48:01 +0200

    chore: upgrade pkgs

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,17 +1,9 @@
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-
-import fetch from "isomorphic-unfetch";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 export function createClient() {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      fetch,
       uri:
         "https://api-eu-central-1.graphcms.com/v2/ckaecqfrf054h01z7h26b71my/master",
     }),
