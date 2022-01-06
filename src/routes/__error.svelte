015b23778883d45fc1e2cff1b387de7e1e66c67f
Julien <julien.lopinto@gmail.com>
2022-01-06 18:13:36 +0100

    Adding 404 route

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	import Container from '$lib/atoms/container/Container.svelte';
+
+	export let title;
+</script>
+
+<Container>
+	<h1>{title}</h1>
+</Container>
